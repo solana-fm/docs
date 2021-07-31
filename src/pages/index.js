b6d8af64f6b814042815f9edfacf62af16eee41d
Fafatoo <fafathur99@gmail.com>
2021-07-31 21:56:16 +0800

    Whole card links to page
    Signed-off-by: Fafatoo <fafathur99@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,17 +16,21 @@ function HomepageHeader() {
       <div className="container">
         <div className="row cards__container">
           <div className={clsx('col col--4', styles.feature)}>
+            <Link 
+            className="navbar__link"to =
+              "https://docs.solana.com/">
             <div className="card">
               <div className="card__header">
                 <h3>⛏ Solana Official Docs</h3>
               </div>
               <div className="card__body">
                 <p>
-                  Click <a href="https://docs.solana.com/"> here</a> to find out
+                  Find out
                   more about Solana's infrastructure.
                 </p>
               </div>
             </div>
+            </Link>
           </div>
         </div>
         <div className="container">
