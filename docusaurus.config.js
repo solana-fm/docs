91c93c70e42bbff2f99be34f0145fac56e7bd077
Fafatoo <fafathur99@gmail.com>
2022-01-07 13:48:01 +0800

    changed docs logo
    Signed-off-by: Fafatoo <fafathur99@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/updated_SolFm.svg',
+  favicon: 'img/transaparent_logo.svg',
   organizationName: 'nozomi-ai', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
   themeConfig: {
@@ -14,7 +14,7 @@ module.exports = {
       title: '',
       logo: {
         alt: 'SolanaFM Logo',
-        src: 'img/updated_SolFm.png',
+        src: 'img/transparent_logo.png',
       },
       items: [
         {
